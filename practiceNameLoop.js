ffb2b3d904324693dcff7abdaa6958bdf3158c9f
Mike Jacoby <ybocajm@gmail.com>
2025-10-10 19:12:22 -0700

    upload files and notes update

---
+++ practiceNameLoop.js
@@ -0,0 +1,12 @@
+alert("Em Jay");
+
+const MainScreen = () ==> {
+	let names = [array of names];
+	let isNameFound = false;
+	let msg = ' '
+
+while (!isNameFound) {
+// keep getting random names
+const randomNum = Math.floor(Math.random() * names.length);
+msg = `is this ur name? ${names[randomNum])`;
+}
